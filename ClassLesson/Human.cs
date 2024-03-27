06d8b61675ec03ed29263f219529cd0e69a383ea
Murad <mrdaydinov@gmail.com>
2024-03-27 13:52:47 +0400

    init commit

---
+++ ClassLesson/Human.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLesson
+{
+    internal class Human
+    {
+        public string Name;
+        public string Surname;
+        public int Age;
+        public string City;
+
+        public Human(string name, string surname, int age, string city)
+        {
+            Name = name;
+            Surname = surname;
+            Age = age;
+            City = city;
+        }
+
+        public bool IsStudent()
+        {
+            if (Age >= 18 && Age <= 25)
+                return true;
+            else
+                return false;
+        }
+
+        public void CheckCity(string city)
+        {
+            if (City == city)
+                Console.WriteLine("Eyni sheherdir");
+            else
+                Console.WriteLine("Deyil");
+        }
+
+        public void CheckCity2(string[] cities)
+        {
+            foreach (string city in cities)
+            {
+                if (City == city)
+                    Console.WriteLine("Eyni sheherdir");
+                else
+                    Console.WriteLine("Deyil");
+            }
+        }
+
+        public void CheckCity2(Human[] humans)
+        {
+            foreach (Human human in humans)
+            {
+                if (human == this)
+                    continue;
+                else if (human.City == City)
+                    Console.WriteLine("Eyni sheherdir");
+                else
+                    Console.WriteLine("Deyil");
+            }
+        }
+
+        public void GetCount(Human[] humans)
+        {
+            Human[] newHumans = new Human[humans.Length];
+
+            for (int i = 0; i < humans.Length; i++)
+            {
+                int count = 0;
+                if (humans[i].IsStudent())
+                {
+                    for (int j = 0; j < humans.Length; j++)
+                    {
+                        if (humans[j].IsStudent())
+                        {
+                            if (humans[i].City == humans[j].City)
+                            {
+                                count++;
+
+                            }
+                        }
+                    }
+                }
+
+                if (!(count == 0))
+                    Console.WriteLine($"count = {count}, city = {humans[i].City}");
+            }
+        }
+    }
+}
---
+++ ClassLesson/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ClassLesson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Human human1 = new("Veli", "Memmedov", 15, "Baku");
+            Human human2 = new("Pirveli", "Memmedov", 20, "Xizi");
+            Human human3 = new("Eli", "Memmedov", 25, "Lerik");
+            Human human4 = new("Gunay", "Memmedova", 32, "Baku");
+            Human human5 = new("Sehran", "Memmedova", 18, "Xizi");
+
+            //Console.WriteLine(human1.IsStudent());
+            //Console.WriteLine(human2.IsStudent());
+            //Console.WriteLine(human3.IsStudent());
+            //Console.WriteLine(human4.IsStudent());
+
+            string[] cities = { human1.City, human2.City, human3.City, human4.City, human5.City };
+
+            Human[] humans = { human1, human2, human3, human4, human5 };
+
+            //human1.CheckCity(human2.City);
+
+            //human1.CheckCity2(humans);
+
+
+            human1.GetCount(humans);
+        }
+    }
+}
